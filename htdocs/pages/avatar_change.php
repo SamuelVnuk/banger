084183de1e40629084fc694bc8b286c103d1b7cd
SamuelVnuk <73060091+SamuelVnuk@users.noreply.github.com>
2022-05-16 18:18:01 +0200

    Add files via upload

---
+++ htdocs/pages/avatar_change.php
@@ -0,0 +1,32 @@
+<?php
+require_once('../scripts/connection.php');
+include('../parts/header.php');
+
+?>
+<style>
+  .container {
+    text-align: center;
+  }
+  h1 {
+    padding-top: 50px;
+  }
+
+</style>
+<?php $message = isset($_GET["message"]) ? $_GET["message"] : "";?>
+<main class>
+  <form action="../scripts/avatar_change_script.php" method="post">
+    <div class="container">
+      <h1>Zmena avatara</h1>
+      <hr>
+      <img src="../images/<?php  echo $avatar;?>"  style="width: 200px">
+      <p><select name="avatar"style="margin-top: 30px;" class="form-select form-select-lg form-select-border-width-2" aria-label=".form-select-lg example">
+          <option disabled selected>Vyberte avatara</option>
+          <option value="jeden.png">User1</option>
+          <option value="dva.png">User2</option>
+          <option value="tri.png">User3</option>
+        </select>
+      <p><button type="submit" class="btn btn-outline-secondary">Zmenit</button>
+      <p class="text-danger"><?php echo $message ?></p>
+    </div>
+  </form>
+</main>
